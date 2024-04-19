cadceb87c1af39cd64207cf53b1af1c33ea3bcc1
swevenau <167485505+swevenau@users.noreply.github.com>
2024-04-19 11:10:57 +1000

    Update settings.js

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
-	title: `RPL Training`,
-	description: `A generic RPL training website.`,
+	title: `RPL Qualified`,
+	description: `An RPL training website.`,
 	url: `https://swevenau.github.io/testing/`, // No trailing slash!
 	name: `RPLQualified`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: false,
