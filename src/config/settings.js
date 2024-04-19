11e184d943c3cdb84f76f8b3eadc47da4791600e
swevenau <167485505+swevenau@users.noreply.github.com>
2024-04-19 12:49:04 +1000

    Update settings.js

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `RPL Qualified`,
+	title: `RPL Training`,
 	description: `An RPL training website.`,
 	url: `https://swevenau.github.io/testing/`, // No trailing slash!
 	name: `RPLQualified`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: false,
-  showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+  showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
