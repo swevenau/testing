aa1155d24191dd19f9c7a0a8d536cfb3731dce7f
swevenau <167485505+swevenau@users.noreply.github.com>
2024-04-19 10:58:13 +1000

    Update footer.js

--- src/config/footer.js
+++ src/config/footer.js
@@ -23,58 +23,12 @@ export const footerSocials = [
 
 export const footerLists = [
 	{
-		title: 'Landing Pages',
-		items: [
-			{
-				title: 'Landing Page 1',
-				slug: '/landing-pages/landing-1',
-			},
-			{
-				title: 'Landing Page 2',
-				slug: '/landing-pages/landing-2',
-			},
-      // {
-			// 	title: 'Landing Page 3',
-			// 	slug: '/landing-pages/landing-3',
-			// },
-		],
-	},
-	{
 		title: 'Company',
 		items: [
 			{
-				title: 'About',
-				slug: '/company/about',
-			},
-			{
-				title: 'Blog',
-				slug: '/blog',
-			},
-			{
 				title: 'Contact',
-				slug: '/company/contact',
+				slug: 'contact',
 			},
-		],
-	},
-	{
-		title: 'Theme',
-		items: [
-			{
-				title: 'Get Started',
-				slug: '/theme/get-started',
-			},
-			{
-				title: 'Style Guide',
-				slug: '/theme/style-guide',
-			},
-      {
-        title: 'Theme Setup',
-        slug: '/theme/theme-setup',
-      },
-      {
-        title: 'Customizing Odyssey',
-        slug: '/theme/customizing-odyssey',
-      }
 		],
 	},
 ];
